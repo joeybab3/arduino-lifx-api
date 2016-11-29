f21ec9326a260efe50aa8330bdebcc9cf440a04b
Joey Babcock <joey_babcock@yahoo.com>
2016-11-29 00:08:31 -0800

    Updates

--- examples/ESP8266/Browser_Controller/Browser_Controller.ino
+++ examples/ESP8266/Browser_Controller/Browser_Controller.ino
@@ -184,9 +184,9 @@ void handleRoot() {
   msg += "<div id=\"container\">\n";
   msg += "<h1>Lifx - Esp8266 Web Controller!</h1>\n";
   msg += "<p id=\"linkholder\"><a href=\"#\" id=\"status\" class='off' onclick=\"togglePower();\"></a> \n";
-  msg += "<a href=\"#\" onclick=\"updateStuff();\"><img src=\"http://joeybabcock.me/iot/hosted/up.png\"/></p>\n";
+  msg += "<a href=\"#\" onclick=\"updateStuff();\"><img src=\"http://joeybabcock.me/iot/hosted/up.png\"/></a></p>\n";
   msg += "<h3>Brightness: <span id=\"brt\">"+String(brightness)+"</span><input type=\"hidden\" id='brightness' value='"+String(brightness)+"' onchange=\"setBrightness(this.value)\"/></h3><br/>\n";
-  msg += "<input type=\"range\" class=\"slider\"  min=\"0\" max=\"99\" value=\""+String(brightness)+"\" name=\"volume-slider\" id=\"volume-slider\" onchange=\"setVolume(this.value)\" />\n";
+  msg += "<input type=\"range\" class=\"slider\"  min=\"0\" max=\"99\" value=\""+String(brightness)+"\" id=\"brightness-slider\" onchange=\"setBrightness(this.value)\" />\n";
   msg += "<p>Server Response:<div id=\"response\" class=\"response\">Setting up...</div></p>\n";
   msg += "<p><form action=\"/\" method=\"get\" id=\"console\"><input placeholder=\"Enter a command...\" type=\"text\" id='console_text'/></form></p>\n";
   msg += "<script>$('#console').submit(function(){parseCmd($(\"#console_text\").val());\nreturn false;\n});getPower();\n</script>\n";
